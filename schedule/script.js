9a01f957ba768573cdadaf29dd45d1abad45e356
splashcard <111317032+Splashcard04@users.noreply.github.com>
2024-02-23 20:00:37 +0000

    more

--- schedule/script.js
+++ schedule/script.js
@@ -3,11 +3,24 @@ const sedan = document.getElementById('sedan')
 const crossover = document.getElementById('crossover')
 const suv = document.getElementById('suv')
 
+const interior = document.getElementById('interior')
+const exterior = document.getElementById('exterior')
+const wheel = document.getElementById('wheel')
+
 form.addEventListener('submit', form => {
     form.preventDefault();
     let vehicle = '';
     if(suv.checked) vehicle = `suv`
     if(sedan.checked) vehicle = `sedan`
     if(crossover.checked) vehicle = `checked`
+
+    let services = [];
+
+    if(interior.checked) services.push('interior')
+    if(exterior.checked) services.push('exterior')
+    if(wheel.checked) services.push('wheel')
+
+
     alert(`you vehicle is ${vehicle}`)
 })
+
